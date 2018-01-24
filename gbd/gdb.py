22c5ff51b3899a047d4072e5a96ff0e4bea247a2
Shota Shimazu <hornet.live.mf@gmail.com>
2018-01-25 00:30:44 +0900

    update

--- gbd/gdb.py
+++ gbd/gdb.py
@@ -1,15 +1,39 @@
 #!/usr/bin/env python3
 
-def main():
-    print("GDB!")
-
+import os
 
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 
+def main():
+    print("GDB!")
+
+
+def read_backups():
+    pass
+
+def create_gitlab_backup():
+    os.system("sudo gitlab-rake gitlab:backup:create")
+
+
 class FileUpload():
 
-    def __init__(self):
+    def __init__(self, save_to):
         self._gauth = GoogleAuth().CommandLineAuth()
         self._drive = GoogleDrive(self._gauth)
+        self._upload_to = save_to
+
+
+    def upload(self):
+        f = self._drive.CreateFile(
+            {
+                'title': 'backup_file',
+                'mimeType': 'application/x-tar',
+                'parents': [
+                    {'kind': 'drive#fileLink', 'id': self._upload_to}
+                ]
+            }
+        )
+        f.SetContentFile('backup_file')
+        f.Upload()
